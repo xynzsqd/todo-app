0ddcc91b1d7adb7ba7b4f2f88bf8f9f371fa0093
xynz <sqdcontrast@gmail.com>
2025-01-16 11:44:48 +0300

    feat(tasks): add function to create tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,7 +14,13 @@ class TaskController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => ['required', 'string', 'max:50'],
+        ]);
+
+        Task::query()->create($validatedData);
+
+        return redirect()->route('task.index');
     }
 
 
