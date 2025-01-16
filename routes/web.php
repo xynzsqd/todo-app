2958e5e68ef1d61021264ee4e7d9c88cff2ff5fd
xynz <sqdcontrast@gmail.com>
2025-01-16 11:27:01 +0300

    fix(routes): add names and change actions

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get('/tasks', [TaskController::class, 'index']);
-Route::post('/tasks', [TaskController::class, 'store']);
-Route::put('/tasks/{task}', [TaskController::class, 'index']);
-Route::delete('/tasks/{task}', [TaskController::class, 'index']);
+Route::get('/tasks', [TaskController::class, 'index'])->name('task.index');
+Route::post('/tasks', [TaskController::class, 'store'])->name('task.store');
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('task.update');
+Route::delete('/tasks/{task}', [TaskController::class, 'delete'])->name('task.delete');
