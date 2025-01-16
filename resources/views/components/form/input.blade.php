f34e1187a484aed8a9c099bf8aa02415ad84d469
xynz <sqdcontrast@gmail.com>
2025-01-16 10:30:05 +0300

    fix: delete placeholder from form.input component

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,2 +1,2 @@
-<input {{ $attributes->class(['text-2xl font-medium py-2 px-3 border-2 rounded-lg bg-slate-50 shadow-sm ']) }}
-    placeholder="Enter task name">
+
+<input {{ $attributes->class(['text-2xl font-medium py-2 px-3 border-2 rounded-lg bg-slate-50 shadow-sm']) }}>
